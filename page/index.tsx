11ead77bc62276eede268ce5b142d9b72fb45973
UzairGhori <ughori435@gmail.com>
2024-09-28 23:52:46 +0500

    first commit

---
+++ page/index.tsx
@@ -0,0 +1,12 @@
+// pages/index.tsx
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Todo App</h1>
+        </div>
+    );
+};
+
+export default Home;
